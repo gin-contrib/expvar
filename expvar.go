5372e8ed144b35d689c08a1b320be10a92839a41
Bo-Yi Wu <appleboy.tw@gmail.com>
2021-06-22 10:42:44 +0800

    chore: update
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- expvar.go
+++ expvar.go
@@ -12,16 +12,16 @@ func Handler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		w := c.Writer
 		c.Header("Content-Type", "application/json; charset=utf-8")
-		w.Write([]byte("{\n"))
+		_, _ = w.Write([]byte("{\n"))
 		first := true
 		expvar.Do(func(kv expvar.KeyValue) {
 			if !first {
-				w.Write([]byte(",\n"))
+				_, _ = w.Write([]byte(",\n"))
 			}
 			first = false
 			fmt.Fprintf(w, "%q: %s", kv.Key, kv.Value)
 		})
-		w.Write([]byte("\n}\n"))
+		_, _ = w.Write([]byte("\n}\n"))
 		c.AbortWithStatus(200)
 	}
 }
